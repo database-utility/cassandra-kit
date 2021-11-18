6fb8df5831728e820ee9af5cc573aa378c587473
Freya Ariel <hifreyaariel@icloud.com>
2021-11-18 18:21:35 +0100

    Add latest version of `libcassandra.xcframework`

--- Compile.sh
+++ Compile.sh
@@ -6,13 +6,14 @@ pushd Build
 git clone https://github.com/leetal/ios-cmake.git
 git clone https://github.com/datastax/cpp-driver.git
 
+# FIXME: `WATCHOS` doesn’t seem to work; using `WATCHOSCOMBINED` for now instead
+# FIXME: `SIMULATORARM64` doesn’t seem to work
 platforms=(
   OS64
   SIMULATOR64
-  SIMULATORARM64
   TVOS
   SIMULATOR_TVOS
-  WATCHOS
+  WATCHOSCOMBINED
   SIMULATOR_WATCHOS
   MAC
   MAC_ARM64
@@ -20,7 +21,7 @@ platforms=(
   MAC_CATALYST_ARM64
 )
 
-# FIXME: `COMBINED` doesn’t seem to work — only produces fat files sometimes 🥴
+# FIXME: `COMBINED` doesn’t seem to work; only produces fat files sometimes 🥴
 #platforms=(
 #  OS64COMBINED
 #  TVOSCOMBINED
@@ -35,7 +36,7 @@ git apply ../../CMakeLists.patch
 for platform in ${platforms[@]}; do
   mkdir -p "build-$platform"
   pushd "build-$platform"
-  cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=$platform -DCMAKE_MACOSX_RPATH=1 -DCASS_BUILD_SHARED=OFF
+  cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=$platform
   cmake --build . --config Release
   # cmake --install . --config Release
   popd
@@ -43,32 +44,34 @@ done
 
 popd
 
+mkdir -p Build/cpp-driver/build-MACCOMBINED/Release
+lipo -create \
+  Build/cpp-driver/build-MAC/Release/libcassandra_static.a \
+  Build/cpp-driver/build-MAC_ARM64/Release/libcassandra_static.a \
+  -output Build/cpp-driver/build-MACCOMBINED/Release/libcassandra_static.a
+
 ls -l Build/cpp-driver/build-*/Release
 
+rm -rf libcassandra.xcframework
 xcodebuild -create-xcframework \
   -library Build/cpp-driver/build-OS64/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
   -library Build/cpp-driver/build-SIMULATOR64/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-SIMULATORARM64/Release/libcassandra_static.a \
-  -headers Build/cpp-driver/include \
   -library Build/cpp-driver/build-TVOS/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
   -library Build/cpp-driver/build-SIMULATOR_TVOS/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-WATCHOS/Release/libcassandra_static.a \
+  -library Build/cpp-driver/build-WATCHOSCOMBINED/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
   -library Build/cpp-driver/build-SIMULATOR_WATCHOS/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-MAC/Release/libcassandra_static.a \
+  -library Build/cpp-driver/build-MACCOMBINED/Release/libcassandra_static.a \
   -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-MAC_ARM64/Release/libcassandra_static.a \
-  -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-MAC_CATALYST/Release/libcassandra_static.a \
-  -headers Build/cpp-driver/include \
-  -library Build/cpp-driver/build-MAC_CATALYST_ARM64/Release/libcassandra_static.a \
-  -headers Build/cpp-driver/include \
-  -output Build/libcassandra.xcframework
+  -output libcassandra.xcframework
+
+#  -library Build/cpp-driver/build-SIMULATORARM64/Release/libcassandra_static.a \
+#  -headers Build/cpp-driver/include \
 
 #xcodebuild -create-xcframework \
 #  -library Build/cpp-driver/build-MAC/Release/libcassandra_static.a \
